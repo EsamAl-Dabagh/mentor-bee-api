3e5c07da9ca16bb3006002d4801cf8d8dd83837b
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-15 16:29:54 +0100

    Fix: rspec file routing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_10_15_150347) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,5 +21,4 @@ ActiveRecord::Schema.define(version: 2018_10_15_150347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
