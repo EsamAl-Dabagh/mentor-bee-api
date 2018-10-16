4b7b0c55e9caac9a9834135584c4c5a0a67d51fa
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-16 13:33:58 +0100

    tests: fix User spec to check for presence of email, password, and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :username, :password, :email
+  validates_presence_of :name, :password, :email
   validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  context "has email, password, and name" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:name) }
+  end
+
+  context "email is unique" do
+    it { should validate_uniqueness_of(:email) }
+  end
 end
