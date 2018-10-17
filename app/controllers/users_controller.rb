7abb68ce85356b0047c7426a1f892bcfd297d386
Freddie <fredjlawson@gmail.com>
2018-10-17 16:01:40 +0100

    chore: refactors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    render json: @users, status: :ok
+    json_response(@users, :ok)
   end
 
   def create
     @user = User.create!(user_params)
-    # When not successful, return exception
-    render json: @user, status: :created
+    json_response(@user, :created)
   end
 
   private
