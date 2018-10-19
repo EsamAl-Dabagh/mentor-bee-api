8eb55701900232a2f65d27440dc5757ecc99eb5d
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-19 17:28:05 +0100

    Fix: sets default url for profile pic on new user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_112528) do
+ActiveRecord::Schema.define(version: 2018_10_19_162441) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +46,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_112528) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.string "pic"
+    t.string "pic", default: "https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
