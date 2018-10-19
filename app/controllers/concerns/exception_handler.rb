8d272831250ccdc851a0146d31d5ea9c4282a69e
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-19 16:14:47 +0100

    Feat: custom error handling to enable rescue for auth

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,31 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
-    rescue_from ActiveRecord::RecordNotFound do |error|
-      json_response({ message: error.message }, :not_found)
-    end
 
     rescue_from ActiveRecord::RecordInvalid do |error|
       json_response({ message: error.message }, :unprocessable_entity)
     end
+
+    rescue_from ActiveRecord::RecordNotFound do |error|
+      json_response({ message: error.message }, :not_found)
+    end
+
+    rescue_from ExceptionHandler::AuthenticationError do |error|
+      json_response({ message: error.message }), :unauthorized)
+    end
+
+    rescue_from ExceptionHandler::MissingToken do |error|
+      json_response({ message: error.message }, :unprocessable_entity)
+    end
+
+    rescue_from ExceptionHandler::InvalidToken do |error|
+      json_response({ message: error.message }, :unprocessable_entity)
+    end
+
   end
 end
