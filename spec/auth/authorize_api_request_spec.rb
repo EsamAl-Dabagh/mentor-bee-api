3c8be83e4f2ef3bea8bfbb583c1bf42ca3bb5a8a
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-20 11:13:16 +0100

    Fix: remove focus from rspec test, linting

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe AuthorizeApiRequest, focus: true do
+RSpec.describe AuthorizeApiRequest do
   let(:user) { create(:user) }
   let(:header) { { "Authorization" => token_generator(user.id) } }
 
--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -1,21 +1,19 @@
 module ControllerSpecHelper
-
   def token_generator(user_id)
     JsonWebToken.encode(user_id: user_id)
   end
 
   def valid_headers
-   {
-     "Authorization" => token_generator(user.id),
-     "Content-Type" => "application/json"
-   }
+    {
+      "Authorization" => token_generator(user.id),
+      "Content-Type" => "application/json"
+    }
  end
 
- def invalid_headers
+  def invalid_headers
     {
       "Authorization" => nil,
       "Content-Type" => "application/json"
     }
-  end
-
+   end
 end
