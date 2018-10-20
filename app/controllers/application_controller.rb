49a422c14c89adc50bfa395eda6aa6bd6a4be258
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-20 17:07:43 +0100

    Feat: API can check if request is authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
