d4b93a4390655b480bcec5b06f518339861d12ab
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-20 17:13:42 +0100

    WIP: refactors App.Controller, adds before_action to ensure authorization; excludes User#create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  def authorize_request
-    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-  end
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+    def authorize_request
+      @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+    end
 end
