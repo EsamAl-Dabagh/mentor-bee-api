c3ff978e843cf4fbfd4869d06dee6156e1f58385
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-22 11:09:17 +0100

    fix: add all origins to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "localhost:5000", "http://mentor-bee.herokuapp.com"
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins "*"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
 end
