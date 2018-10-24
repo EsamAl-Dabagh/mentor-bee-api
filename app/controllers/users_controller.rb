904ca7a965a42d56a5623f9e3fb170af61f8b577
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-10-24 22:34:32 +0100

    tiny linting fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize_request, only: [:create]
+  skip_before_action :authorize_request, only: :create
 
   def index
     users = User.all
